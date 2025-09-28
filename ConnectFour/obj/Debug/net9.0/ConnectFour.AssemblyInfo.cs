c80b061477edb7afcf5f7f51175cd41150b05e49
Walter Stamm <walterstamm92@gmail.com>
2025-09-28 16:31:55 +0200

    update

--- ConnectFour/obj/Debug/net9.0/ConnectFour.AssemblyInfo.cs
+++ ConnectFour/obj/Debug/net9.0/ConnectFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc509ff833b92519f51ec498e5ea4bc7d3fdfada")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
